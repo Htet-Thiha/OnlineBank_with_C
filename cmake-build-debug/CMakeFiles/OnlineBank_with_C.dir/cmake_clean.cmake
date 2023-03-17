4b17b0b05f45004748a5e37ffdd1e7acbc16b2e4
Htet-Thiha <120123935+Htet-Thiha@users.noreply.github.com>
2023-03-18 04:00:07 +0630

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/OnlineBank_with_C.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OnlineBank_with_C.dir/main.c.o"
+  "CMakeFiles/OnlineBank_with_C.dir/main.c.o.d"
+  "OnlineBank_with_C"
+  "OnlineBank_with_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OnlineBank_with_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,35 @@
+//
+// Created by National Cyber City on 2/6/2023.
+//
+
+
+#include "stdio.h"
+#include "zoom_online_bank.h"
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+//get_time();
+//    current_data_toTransfer(3000);
+//    calculate_amounts_same_days(0);
+
+//    get_time();
+//    printf("current edited time :%s",Ctime[0].c_time);
+
+//    integer_to_char(1000);
+//long long int aa=0;
+//int a[7]={0,3,4,5,6,7,8,0};
+//scanf("%llu",&aa);
+//
+//printf("%lu",aa);
+//    phone_format_check=-1;
+//    phone_format_validation(aa);
+//    printf("%d",phone_format_check);
+//    int amount =100;
+//    amount = amount + (amount/10);
+//    printf("%d",amount);
+
+    return 0;
+}
